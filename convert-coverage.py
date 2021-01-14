9530fafcda48ee88af8fb30224d44d24be8b2460
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-01-14 15:09:21 +0100

    ci: add coverage report (#33)
    * tests: add pytest-cov
    
    * ci: upload xml coverage report as action artifact
    
    * feat: add script to convert coverage report so that it can be used locally
    
    * fixup! feat: add script to convert coverage report so that it can be used locally

---
+++ convert-coverage.py
@@ -0,0 +1,13 @@
+"""Convert a cov.xml report so that it is usable locally"""
+import os.path
+
+from lxml import etree as et  # type: ignore
+
+cov = et.parse("cov.xml")
+
+[source] = et.XPath("//source")(cov)
+
+source.text = os.path.abspath("sansio_lsp_client")
+
+with open("cov.xml", "wb") as f:
+    f.write(et.tostring(cov, xml_declaration=True))
