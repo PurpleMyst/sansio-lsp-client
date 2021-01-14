2c81a19aea57d55df5477f9aba1cbc27078df68c
Akuli <akuviljanen17@gmail.com>
2021-01-14 10:12:35 +0200

    tests: more tests (#21)
    * add javascript-typescript-langserver test
    
    * test javascript-typescript-langserver
    
    * copy/pasta --> function
    
    * add clangd test (currently it fails)
    
    * make comment readable while keeping black happy
    
    * add clangd 10
    
    * use bash
    
    * fix var

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
 import contextlib
+import pathlib
+import platform
+import shutil
 import subprocess
 import sys
+
+import pytest
 
 import sansio_lsp_client as lsp
 
 
 @contextlib.contextmanager
-def run_stdio_langserver(project_root, command):
+def run_langserver(project_root, command):
     with subprocess.Popen(
         command, stdin=subprocess.PIPE, stdout=subprocess.PIPE
     ) as process:
@@ -26,20 +31,50 @@ def run_stdio_langserver(project_root, command):
         yield (lsp_client, make_event_iterator())
 
 
-def test_pyls(tmp_path):
-    foo_uri = (tmp_path / "foo.py").as_uri()
+def do_stuff_with_a_langserver(tmp_path, filename, file_content, command):
+    path = tmp_path / filename
+    path.write_text(file_content)
 
-    with run_stdio_langserver(tmp_path, [sys.executable, "-m", "pyls"]) as (
-        lsp_client,
-        event_iter,
-    ):
+    with run_langserver(tmp_path, command) as (lsp_client, event_iter):
         inited = next(event_iter)
         assert isinstance(inited, lsp.Initialized)
         lsp_client.did_open(
             lsp.TextDocumentItem(
-                uri=foo_uri,
+                uri=path.as_uri(),
                 languageId="python",
-                text="""\
+                text=file_content,
+                version=0,
+            )
+        )
+
+        diagnostics = next(event_iter)
+        assert isinstance(diagnostics, lsp.PublishDiagnostics)
+        assert diagnostics.uri == path.as_uri()
+
+        event_id = lsp_client.completions(
+            text_document_position=lsp.TextDocumentPosition(
+                textDocument=lsp.TextDocumentIdentifier(uri=path.as_uri()),
+                position=lsp.Position(
+                    # first line = 0, first column = 0
+                    line=file_content.count("\n"),
+                    character=len(file_content.split("\n")[-1]),
+                ),
+            ),
+            context=lsp.CompletionContext(
+                triggerKind=lsp.CompletionTriggerKind.INVOKED
+            ),
+        )
+        completions = next(event_iter)
+        assert completions.message_id == event_id
+
+        return (diagnostics, completions)
+
+
+def test_pyls(tmp_path):
+    diagnostics, completions = do_stuff_with_a_langserver(
+        tmp_path,
+        "foo.py",
+        """\
 import sys
 def do_foo():
     pass
@@ -47,34 +82,110 @@ def do_bar():
     pass
 
 do_""",
-                version=0,
-            )
-        )
+        [sys.executable, "-m", "pyls"],
+    )
 
-        diagnostics = next(event_iter)
-        assert isinstance(diagnostics, lsp.PublishDiagnostics)
-        assert diagnostics.uri == foo_uri
-        assert [diag.message for diag in diagnostics.diagnostics] == [
-            "'sys' imported but unused",
-            "undefined name 'do_'",
-            "E302 expected 2 blank lines, found 0",
-            "E302 expected 2 blank lines, found 0",
-            "W292 no newline at end of file",
-            "E305 expected 2 blank lines after class or function definition, found 1",
-        ]
+    assert [diag.message for diag in diagnostics.diagnostics] == [
+        "'sys' imported but unused",
+        "undefined name 'do_'",
+        "E302 expected 2 blank lines, found 0",
+        "E302 expected 2 blank lines, found 0",
+        "W292 no newline at end of file",
+        "E305 expected 2 blank lines after class or function definition, found 1",
+    ]
 
-        event_id = lsp_client.completions(
-            text_document_position=lsp.TextDocumentPosition(
-                textDocument=lsp.TextDocumentIdentifier(uri=foo_uri),
-                position=lsp.Position(line=6, character=3),
-            ),
-            context=lsp.CompletionContext(
-                triggerKind=lsp.CompletionTriggerKind.INVOKED
-            ),
-        )
-        completion = next(event_iter)
-        assert completion.message_id == event_id
-        assert [item.label for item in completion.completion_list.items] == [
-            "do_bar()",
-            "do_foo()",
-        ]
+    assert [item.label for item in completions.completion_list.items] == [
+        "do_bar()",
+        "do_foo()",
+    ]
+
+
+test_langservers = (
+    pathlib.Path(__name__).absolute().parent / "test_langservers"
+)
+
+
+@pytest.mark.skipif(
+    not (
+        test_langservers / "node_modules/.bin/javascript-typescript-stdio"
+    ).exists(),
+    reason="javascript-typescript-langserver not found",
+)
+@pytest.mark.skipif(
+    shutil.which("node") is None, reason="node not found in $PATH"
+)
+def test_javascript_typescript_langserver(tmp_path):
+    diagnostics, completions = do_stuff_with_a_langserver(
+        tmp_path,
+        "foo.js",
+        """\
+function doSomethingWithFoo(x, y) {
+    const blah = x + y;
+    return asdf asdf;
+}
+
+doS""",
+        [test_langservers / "node_modules/.bin/javascript-typescript-stdio"],
+    )
+    assert [diag.message for diag in diagnostics.diagnostics] == [
+        "';' expected."
+    ]
+    assert "doSomethingWithFoo" in [
+        item.label for item in completions.completion_list.items
+    ]
+
+
+def clangd_decorator(version):
+    def inner(function):
+        function = pytest.mark.skipif(
+            platform.system() == "Windows",
+            reason="don't know how clangd works on windows",
+        )(function)
+        function = pytest.mark.skipif(
+            not list(test_langservers.glob(f"clangd_{version}.*")),
+            reason=f"clangd {version} not found",
+        )(function)
+        return function
+
+    return inner
+
+
+c_code = """\
+#include <stdio.h>
+void do_foo(void) {
+}
+int do_bar(char x, long y) {
+    short z = x + y;
+}
+
+do_"""
+
+
+@clangd_decorator(10)
+def test_clangd_10(tmp_path):
+    diagnostics, completions = do_stuff_with_a_langserver(
+        tmp_path,
+        "foo.c",
+        c_code,
+        [next(test_langservers.glob("clangd_10.*")) / "bin" / "clangd"],
+    )
+    assert [diag.message for diag in diagnostics.diagnostics] == [
+        "Non-void function does not return a value",
+        "Unknown type name 'do_'",
+        "Expected identifier or '('",
+    ]
+    # TODO: why does do_bar not show up in completions?
+
+
+@pytest.mark.xfail
+@clangd_decorator(11)
+def test_clangd_11(tmp_path):
+    diagnostics, completions = do_stuff_with_a_langserver(
+        tmp_path,
+        "foo.c",
+        c_code,
+        [next(test_langservers.glob("clangd_11.*")) / "bin" / "clangd"],
+    )
+    # TODO
+    # assert diagnostics.diagnostics == [...]
+    # assert [item.label for item in completions.completion_list.items] == [...]
